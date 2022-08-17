6cf780a56130037bb9ef51df1a148c0057647caf
aa196883 <aaly@enssat.fr>
2022-08-17 10:46:44 +0200

    first

---
+++ src/app/request-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Parking } from './Parking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestServiceService {
+  array: Parking[] = [];
+
+  constructor() {
+    this.array.push(new Parking('B2', 'Tom Smith', 'Available', '', 3));
+    this.array.push(new Parking('C4', 'Alice Miller', 'Booked', 'Marc Vercammen', 2));
+    this.array.push(new Parking('D9', 'Bob Jackson', 'Booked', 'Sarah Reese', 0));
+    this.array.push(new Parking('E10', 'Marc Vercammen', 'Released', '', 1));
+    this.array.push(new Parking('P1', 'Marc Vercammen', 'Released', '', 1));
+  }
+
+  getArray(): Parking[]{
+    return this.array;
+  }
+}
