8d93aa35e2176c4b0e5633b9c3065ad31dae7d9f
yash <yashasvi_appadoo@hotmail.com>
2022-08-18 14:39:44 +0400

    modal works

--- src/app/modals/cancel-booking/cancel-booking.component.ts
+++ src/app/modals/cancel-booking/cancel-booking.component.ts
@@ -3,20 +3,21 @@ import { SdwdsModalService } from '@sdworx/sdwds-modal';
 import { ModalFooterButtonsComponent } from 'src/app/buttons/modal-footer-buttons/modal-footer-buttons.component';
 //your custom component needs to import the NgbActiveModal service from ng-bootstrap
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalTemplateComponent } from '../modal-template/modal-template.component';
 
 @Component({
   selector: 'app-cancel-booking',
   templateUrl: './cancel-booking.component.html',
   styleUrls: ['./cancel-booking.component.css']
 })
-export class CancelBookingComponent{
+export class CancelBookingComponent extends ModalTemplateComponent{
 
-  constructor(public activeModal: NgbActiveModal) {}
-
-  ok(): void {
-    //close the current modal with a specific return value
-    //this triggers the subscription we have on the modal in our typescript code
-    this.activeModal.close(true);
+  constructor( activeModal: NgbActiveModal) {
+    super(activeModal)
+    super.modalHeader = "Cancel Booking"
+    super.modalBody = "Are you sure you want to cancel this booking?\n\n In case you change your mind later, you will need to create another request which might not be approved."
+    super.modalOrange = "Yes"
+    super.modalWhite = "No"
   }
 
 }
